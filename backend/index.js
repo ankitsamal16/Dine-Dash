b6e9c19578a4aff829f12599dd3befd98b9ab763
AnkitSamal16 <126022706+AnkitSamal16@users.noreply.github.com>
2024-06-07 20:28:36 +0530

    vercel.json

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,11 @@ const foodRouter = require("./routes/foodRoute");
 const userRouter = require("./routes/userRoute");
 const cartRouter = require("./routes/cartRoute");
 const orderRouter = require("./routes/orderRoute");
+const corsConfig = {
+    origin: "*",
+    credential: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+};
 require("dotenv").config();
 
 
@@ -22,7 +27,7 @@ const PORT = 4000
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsConfig));
 
 
 
